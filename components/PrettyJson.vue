db6f8a5106be5d882b6a1f3ecaf8f51e8129eca6
rohrig <richard.t.rohrig@gmail.com>
2023-04-25 13:54:24 +0200

    feat: pretty json

---
+++ components/PrettyJson.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box">
+    <!-- <JsonViewer :value="jsonData" copyable boxed sort theme="light"  @onKeyClick="keyClick"/> -->
+    <h4>Response</h4>
+    <JsonViewer class="min-h-[800px] min-w-[500px]" :value="jsonData" expandDepth="5" expanded copyable boxed sort theme="dark"  @onKeyClick="keyClick"/>
+  </div>
+</template>
+
+<script setup>
+import {JsonViewer} from "vue3-json-viewer"
+// if you used v1.0.5 or latster ,you should add import "vue3-json-viewer/dist/index.css"
+import "vue3-json-viewer/dist/index.css";
+import { reactive, ref } from "vue";
+
+
+
+const jsonData = useState('methodResult', () => 'waiting ...');
+
+// const jsonData = reactive(props.obj);
+const keyClick = (keyName)=>{
+  console.log(keyName,"it was click")
+}
+</script>
+
+<style>
+.box{
+  margin-top: 1rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,6 @@
 <template>
   <div class="flex justify-center items-center h-screen m-10">
-    
-    <div class="w-[500px] bg-neutral-100">
-      <div> {{str}}</div>
-    </div>
-    
+    <PrettyJson />
   </div>
 </template>
 
-<script lang="ts" setup>
-import { SfLink } from '@storefront-ui/vue';
-const router = useRouter()
-const routes = router.getRoutes()
-
-
-const res = useState('methodResult', () => 'waiting ...');
-var str = computed(() => JSON.stringify(res.value,null,2));
-
-
-</script>
-
-<style>
-pre {outline: 1px solid #ccc; padding: 5px; margin: 5px; }
-.string { color: green; }
-.number { color: darkorange; }
-.boolean { color: blue; }
-.null { color: magenta; }
-.key { color: rgb(5, 36, 14); }
-
-</style>
