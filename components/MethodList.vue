86f0b13655988537374a0ab952809b2f66feba59
rohrig <richard.t.rohrig@gmail.com>
2023-05-03 10:16:04 +0200

    feat: nest method pages into method dir

--- components/MethodList.vue
+++ components/MethodList.vue
@@ -1,25 +1,18 @@
 <template>
- 
     <div class=" ">
-      <div v-for="method in methods">
-      <div v-if="method.id !== '/'" class=" flex p-4 bg-white border-neutral-300 border-b">
-        <NuxtLink :to="'/' + method.name" class="flex flex-row flex-nowrap">
-          <SfButton
-            aria-label="open-method"
-            variant="tertiary"
-            square
-          >
-            <template #prefix>
-              <Component :is="SfIconOpenInNew" />
-            </template>
-            <span class="hidden md:inline-flex">{{ method.name }} </span>
-          </SfButton>
-        </NuxtLink>
-      </div>
+        <div v-for="method in menuRoutes">
+            <div class=" flex p-4 bg-white border-neutral-300 border-b">
+                <NuxtLink :to="method.path" class="flex flex-row flex-nowrap">
+                    <SfButton aria-label="open-method" variant="tertiary" square>
+                        <template #prefix>
+                            <Component :is="SfIconOpenInNew" />
+                        </template>
+                        <span class="hidden md:inline-flex">{{ method.name }} </span>
+                    </SfButton>
+                </NuxtLink>
+            </div>
+        </div>
     </div>
-  </div>
-  
-  
 </template>
 
 <script lang="ts" setup>
@@ -27,34 +20,46 @@ import { SfAccordionItem, SfIconOpenInNew } from '@storefront-ui/vue';
 import { ref } from 'vue';
 import { SfLink } from '@storefront-ui/vue';
 import { SfButton } from '@storefront-ui/vue';
-import {sdk} from '~/sdk.config';
+import { sdk } from '~/sdk.config';
 
 const res = useState('methodResult')
 
 async function callEndpoint(methodName: string) {
-  //ts ignore is needed because the method name is dynamic
-  // @ts-ignore
-const {data} = await sdk.boilerplate[methodName]('test');
-res.value = data
+    //ts ignore is needed because the method name is dynamic
+    // @ts-ignore
+    const { data } = await sdk.boilerplate[methodName]('test');
+    res.value = data
 }
 
+type MethodType = {
+    id: string;
+    name: string;
+};
 function reset() {
-res.value = 'waiting to call getCart() ...'
+    res.value = 'waiting to call getCart() ...'
 }
 
-const opened = ref<boolean[]>([]);
- 
+const opened = ref<MethodType[]>([]);
+
 const router = useRouter()
 const routes = router.getRoutes()
-
+const menuRoutes = useState(() => [{
+    path: '/methods/exampleMethod',
+    name: 'exampleMethod'
+}])
 
 //add routes to accordionItems
-const methods = routes.map((route) => {
-  return {
-    id: route.path,
-    name: route.path.replace("/", ""),
-  }
+routes.map((route) => {
+    if (route.path.includes("/methods") && !inRouteArray(route.path)) {
+        menuRoutes.value.push({
+            path: route.path,
+            name: route.path.replace("/methods/", ""),
+        })
+    }
 })
 
+function inRouteArray(route: string) {
+    return menuRoutes.value.some((item) => item.path === route)
+}
 
 </script>
